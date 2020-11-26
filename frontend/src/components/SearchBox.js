02e189b991d1f943b84377c0198db9b3c5af7878
German De Bonis <germandebonis@Santiagos-MacBook-Pro.local>
2020-11-26 10:09:11 -0300

    css changes

--- frontend/src/components/SearchBox.js
+++ frontend/src/components/SearchBox.js
@@ -21,7 +21,11 @@ const SearchBox = ({ history }) => {
         placeholder="Search Products..."
         className="mr-sm-2 ml-sm-5"
       ></Form.Control>
-      <Button type="submit" variant="outline-success" className="p-2">
+      <Button
+        type="submit"
+        variant="outline-success"
+        className="p-2 mt-2 mt-md-0"
+      >
         Search
       </Button>
     </Form>
