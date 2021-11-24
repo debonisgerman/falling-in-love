3636123299f656960e00460c129136aa0df67826
Germán <debonis.german@gmail.com>
2021-11-23 23:46:46 -0500

    mailer

--- backend/utils/mailer.js
+++ backend/utils/mailer.js
@@ -51,6 +51,40 @@ const sendMail = async (data, order, orderItems) => {
         </div>
       </div>
     `,
+    text: `
+      <div>
+        <h3>Pedido por el sitio web</h3>
+        <div>Datos del cliente:</div>
+        <div>
+          <ul>
+              <li><strong>Nombre: </strong> ${data.name}</li>
+              <li><strong>Email: </strong> ${data.email}</li>
+              <li><strong>Dirección:</strong> ${data.address}</li>
+              <li><strong>Provincia:</strong> ${data.province}</li>
+              <li><strong>Distrito:</strong> ${data.district}</li>
+              <li><strong>Teléfono:</strong> ${data.phone}</li>
+              <li><strong>Factura:</strong> ${data.bill ? 'Si' : 'No'}</li>
+              <li><strong>Razón Social:</strong> ${data.socialReason ? data.socialReason : '-'}</li>
+              <li><strong>RUC:</strong> ${data.ruc ? data.ruc : '-'}</li>
+          </ul>
+        </div>
+        <div>
+          <div>Datos del pedido:</div>
+          <ul>
+                <li>Nro. Orden: ${order.billNumber}</li>
+            ${orderItems.map((item) => {
+      return `
+                <li>Producto: ${item.name}</li>
+                <li>Color: ${item.colorName}</li>
+                <li>Talla: ${item.sizeName}</li>
+                <li>Cantidad: ${item.qty}</li>
+                <hr />
+              `;
+    })}
+          </ul>
+        </div>
+      </div>
+    `,
   });
 
   await transporter.sendMail({
@@ -58,6 +92,40 @@ const sendMail = async (data, order, orderItems) => {
     to: data.email,
     subject: `${data.name} gracias por pedir en Falling In love`,
     html: `
+      <div>
+        <h3>Pedido por el sitio web</h3>
+        <div>Datos del cliente:</div>
+        <div>
+          <ul>
+              <li><strong>Nombre: </strong> ${data.name}</li>
+              <li><strong>Email: </strong> ${data.email}</li>
+              <li><strong>Dirección:</strong> ${data.address}</li>
+              <li><strong>Provincia:</strong> ${data.province}</li>
+              <li><strong>Distrito:</strong> ${data.district}</li>
+              <li><strong>Teléfono:</strong> ${data.phone}</li>
+              <li><strong>Factura:</strong> ${data.bill ? 'Si' : 'No'}</li>
+              <li><strong>Razón Social:</strong> ${data.socialReason ? data.socialReason : '-'}</li>
+              <li><strong>RUC:</strong> ${data.ruc ? data.ruc : '-'}</li>
+          </ul>
+        </div>
+        <div>
+          <div>Datos del pedido:</div>
+          <ul>
+                <li>Nro. Orden: ${order.billNumber}</li>
+            ${orderItems.map((item) => {
+      return `
+                <li>Producto: ${item.name}</li>
+                <li>Color: ${item.colorName}</li>
+                <li>Talla: ${item.sizeName}</li>
+                <li>Cantidad: ${item.qty}</li>
+                <hr />
+              `;
+    })}
+          </ul>
+        </div>
+      </div>
+    `,
+    text: `
       <div>
         <h3>Pedido por el sitio web</h3>
         <div>Datos del cliente:</div>
